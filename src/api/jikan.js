25f2174722615398cbfbbdee15dc9a5ca28fb5c4
RONIT <balironit@gmail.com>
2025-04-14 22:48:47 +0530

    feat: update index.html title and favicon; add API fetch function; implement Home page with AnimeCard; create Navbar and AnimeCard components; remove unused Stats component

---
+++ src/api/jikan.js
@@ -0,0 +1,5 @@
+export const fetchupcominganimedata = async() => {
+    const res = await fetch('https://api.jikan.moe/v4/seasons/upcoming');
+    const data = await res.json();
+    return data.data
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchupcominganimedata } from '../api/jikan'
+import AnimeCard from '../components/AnimeCard';
 
 const Home = () => {
+   const [animelist,setAnimelist] = useState([]);
+
+   useEffect(()=>{
+     fetchupcominganimedata().then(setAnimelist);
+     console.log(animelist)
+   },[])
   return (
-    <div>Home</div>
+   <section>
+    <div>
+      <h1 className='text-blue-300'>Upcominganime</h1>
+      {animelist.map((anime) => (
+        <AnimeCard key={anime.id} anime={anime}/>
+      ))};
+    </div>
+   </section>
   )
 }
 
